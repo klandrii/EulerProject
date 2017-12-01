56c5f89e7862dac127310b82de5e06754282b27b
Andrii <demonictoast@gmail.com>
2017-12-01 15:48:10 +0200

    Euler Project problems

--- EulerProject/Program.cs
+++ EulerProject/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace EulerProject
 {
+    
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            //Bruteforce solution, ill try checking all ints from 1 to 1000 for being multiples of 3 and 5
+            int result = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if (((i % 3) == 0) || ((i % 5) == 0))
+                {
+                    result += i;// sum of all multiples
+                }
+            }
+            Console.WriteLine(result);
+            Clipboard.SetText(Convert.ToString(result));// now ill copy our result to clipboard, for for convenience sake
         }
     }
 }
