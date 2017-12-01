635b1993e0f10626e01a3ddc85e076ab0c6301da
Andrii <demonictoast@gmail.com>
2017-12-01 18:55:31 +0200

    problem 9

---
+++ EulerProject9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EulerProject9
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            //Here we need  to find a poduct of abc, knowing that a^2+b^2=C^2 , a<b<c and a+b+c=1000
+            int sum = 1000;
+            for (int a = 1; a <sum/3; a++)
+            {
+                for (int b = a; b < sum/2; b++)
+                {
+                    int c = sum-(a+b);
+                    if (a*a + b*b ==c*c)
+                    {
+                        int result = a * b * c;
+                        Console.WriteLine(result);
+                        Clipboard.SetText(Convert.ToString(result));
+                    }
+                }
+            }
+        }
+    }
+}
