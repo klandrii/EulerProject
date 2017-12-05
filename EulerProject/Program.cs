aba9698850fe8d456fe662764e9e7398ec35e342
Andrii <demonictoast@gmail.com>
2017-12-05 18:14:47 +0200

    Problem 10 solved

--- EulerProject/Program.cs
+++ EulerProject/Program.cs
@@ -1,21 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EulerProject
 {
-    
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            var instance1= new Euler1();
+            var instance1 = new Euler1();
             var instance2 = new Euler9();
-            Console.WriteLine("Problem #1 : {0}",instance1.Answer());
-            Console.WriteLine("Problem #2 : {0}",instance2.Answer());
+            var instance3 = new Euler10();
+            Console.WriteLine("Problem #1 : {0}", instance1.Answer());
+            Console.WriteLine("Problem #9 : {0}", instance2.Answer());
+            Console.WriteLine("Problem #10 : {0}", instance3.Answer());
         }
     }
-    class Euler1
+
+    internal class Euler1
     {
         private int result = 0;
+
         public string Answer()
         {
             for (int i = 1; i < 1000; i++)
@@ -28,7 +33,8 @@ namespace EulerProject
             return Convert.ToString(result);
         }
     }
-    class Euler9
+
+    internal class Euler9
     {
         private int sum = 1000;
         private string wrong = "wrong";
@@ -37,19 +43,49 @@ namespace EulerProject
         {
             for (int a = 1; a < sum / 3; a++)
             {
-                for (int b = a; b < sum/2; b++)
+                for (int b = a; b < sum / 2; b++)
                 {
                     int c = sum - (a + b);
-                    if (a*a + b*b == c*c)
+                    if (a * a + b * b == c * c)
                     {
                         int result = a * b * c;
                         return Convert.ToString(result);
                     }
                 }
-                
             }
 
             return wrong;
+        }
+    }
+
+    internal class Euler10
+    {
+        //Find sum for all primes bellow two million
+        int n = 1000000;
+        bool[] sieve = new bool[2000000];
+        List<double> primes = new List<double>();
+        public string Answer()
+        {
+            for (int i = 2; i < sieve.Length; i++)
+            {
+                sieve[i] = true;
+            }
+            for (int i = 2; Math.Pow(i,2)< sieve.Length; i++)  // removing all multiples of 2 
+            {
+                for (int j = (int)Math.Pow(i,2); j < sieve.Length; j+=i)//all multiples of >2 are not primes
+                {
+                    sieve[j] = false;
+                }
+            }
+            for (int i = 2; i < sieve.Length; i++)
+            {
+                if (sieve[i])
+                {
+                    primes.Add(i);
+                }
+            }
+            string result = primes.Sum().ToString();
+            return result;
         }
     }
 }
