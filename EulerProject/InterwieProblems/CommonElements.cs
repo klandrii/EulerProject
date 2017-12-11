476b08104866be64441ec1284aef01dcf63f00bf
Andrii <demonictoast@gmail.com>
2017-12-11 19:41:07 +0200

    Interwiev Quest2
    New problem solved
    Logger message changed

---
+++ EulerProject/InterwieProblems/CommonElements.cs
@@ -0,0 +1,42 @@
+﻿using EulerProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterwiewQuestions
+{
+    //---------------------------------------------------------------------
+    //Compare two  descending arrays of unknown size and find all common elements
+    //---------------------------------------------------------------------
+    class CommonElements
+    {
+        private int[] a = new int[] {1, 3, 4, 6, 7, 9};//sample arrays
+        private int[] b = new int[] {1, 2, 4, 5, 9, 10};
+        List<int> result = new List<int>();
+
+        public string Answer()// First way to solve this problem using Enumerable.Intersect method
+        {
+            var intersection = a.Intersect(b);
+            Logger.Log.Info("I'm using Enumerable.Intersect");
+            return String.Join(",", intersection);
+        }
+
+        public string AnswerPure() // Let's just pretend that intersect method does not exist
+        {
+            for (int i = 0; i < a.Length; i++)// Ok it's time for some bruteforce
+            {
+                foreach (int item in b)
+                {
+                    if (a[i]==item)
+                    {
+                        result.Add(item);
+                    }
+                }
+            }
+            Logger.Log.Info("I'm using custom made method");
+            return String.Join(",", result);
+        }
+    }
+}
--- EulerProject/Program.cs
+++ EulerProject/Program.cs
@@ -13,11 +13,14 @@ namespace EulerProject
             var instance3 = new EulerProblems.Euler10();
             var instance4 = new InterwiewQuestions.MostFrequentArray();
             var instance5 = new EulerProblems.Euler11();
+            var instance6 =new InterwiewQuestions.CommonElements();
             Console.WriteLine("Euler problem #1 : {0}", instance1.Answer());
             Console.WriteLine("Euler problem #9 : {0}", instance2.Answer());
             Console.WriteLine("Euler problem #10 : {0}", instance3.Answer());
             Console.WriteLine("Interview problem #1 : {0}", instance4.Answer());
-            Console.WriteLine("Euler problem#11: {0}",instance5.Answer());
+            Console.WriteLine("Euler problem #11: {0}",instance5.Answer());
+            Console.WriteLine("Interview problem #2 : {0}", instance6.Answer());
+            //Console.WriteLine("Interview problem #2 : {0}", instance6.AnswerPure());
             Console.ReadLine();
         }
     }
